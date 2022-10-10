dfb45fe74279ae6e6707115fdbcb7d3784c41ff1
Jason171096 <bboyace10@gmail.com>
2022-10-09 23:00:01 -0500

    Implement Auth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,12 +3,14 @@ import {
   MagnifyingGlassIcon,
   BuildingLibraryIcon,
   PlusCircleIcon,
+  HeartIcon,
+  RssIcon
 } from "@heroicons/react/24/solid";
 
 const Sidebar = () => {
   return (
-    <div>
-      <div className="text-gray-500">
+    <div className="text-gray-500 py-5">
+      <div className="space-y-3">
         <div>
           <button className="flex items-center space-x-2 hover:text-white">
             <HomeIcon className="w-5 h-5" />
@@ -16,7 +18,6 @@ const Sidebar = () => {
           </button>
         </div>
         <div>
-            
           <button className="flex items-center space-x-2 hover:text-white">
             <MagnifyingGlassIcon className="w-5 h-5" />
             <p>Search</p>
@@ -28,7 +29,33 @@ const Sidebar = () => {
             <p>Your Library</p>
           </button>
         </div>
-      </div>
+      
+      <hr className="border-1"/>
+      <div>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <PlusCircleIcon className="w-5 h-5" />
+            <p>Create Playlist</p>
+          </button>
+        </div>
+        <div>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <HeartIcon className="w-5 h-5" />
+            <p>Like Songs</p>
+          </button>
+        </div>
+        <div>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <RssIcon className="w-5 h-5" />
+            <p>Your episodes</p>
+          </button>
+        </div>
+        <hr className="border-1"/>
+        </div>
+        
+        {/* PlayList */}
+
+        <div><p className="cursor-pointer hover:text-white">Playlist name...</p></div>
+        
     </div>
   )
 }
